bf9ddaa208a528ca0ccd46b2b3e1f09177b29f0f
shrangi <35887384+shrangi@users.noreply.github.com>
2018-10-01 11:05:35 +0530

    update 3 again

--- config.js
+++ config.js
@@ -1,12 +1,4 @@
 
-// exports.key =  {
-
-//   consumer_key: 'cbGINqqSu9SOtXvOcc1zFs6oV',
-//   consumer_secret: 'iUH5pVwisYTyqdEpTM6pUxwb8mb4DiJYKJQlGwNsFXMNPbytdF',
-//   access_token_key: '1042062259550158848-B3QvqXcPeY7LzPNGGGSWjBwxSrenmn',
-//   access_token_secret: 'QO2KlSXytTDBkTRXwi6CVkAWTsSQUUwFzXPfo1toqoj68'
-
-// }
 
 exports.key =  {
 
