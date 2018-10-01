005e19c0b15c16e9b644c6f4cf45f2344b4dca12
shrangi <35887384+shrangi@users.noreply.github.com>
2018-10-01 11:17:32 +0530

    inverted's are removed

--- config.js
+++ config.js
@@ -2,9 +2,9 @@
 
 exports.key =  {
 
-  consumer_key: 'process.env.CONSUMER_KEY',
-  consumer_secret: 'process.env.CONSUMER_SECRET',
-  access_token_key: 'process.env.ACCESS_TOKEN_KEY',
-  access_token_secret: 'process.env.ACCESS_TOKEN_SECRET'
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
 
 }
